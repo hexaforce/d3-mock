3c3b0c1f65a2f7ecef921f2b0aec0cbbb799d23f
tomokazu.tantaka <tantaka.t@fintech.dmm.com>
2019-06-07 12:09:26 +0900

    test

--- src/components/ExpansionPanel.vue
+++ src/components/ExpansionPanel.vue
@@ -15,16 +15,52 @@
       </v-flex>
       <v-flex xs12 lg5 offset-lg2>
         <v-expansion-panel inset>
-          <v-expansion-panel-content v-for="(item,i) in 5" :key="i" >
+          <draggable v-model='cards'>
+          <v-expansion-panel-content v-for='card in cards' :key='card.id' ripple>
             <template v-slot:header>
-              <div>Item</div>
+              <div>{{card.Title}}</div>
             </template>
             <v-card>
               <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
             </v-card>
           </v-expansion-panel-content>
+          </draggable>
         </v-expansion-panel>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  name: 'v-tabs',
+  components: {
+    draggable
+  },
+  methods: {
+  },
+  data () {
+    return {
+      active: 0,
+      cards: [
+        {
+          id: 1,
+          Title: '1st Card',
+          text: 'This is a 1st card'
+        },
+        {
+          id: 2,
+          Title: '2nd Card',
+          text: 'This is a 2nd card'
+        },
+        {
+          id: 3,
+          Title: '3rd Card',
+          text: 'This is a 3rd card'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,19 @@
 <template>
-  <ExpansionPanel />
+  <div>
+    <ExpansionPanel />
+    <!-- <Draggable /> -->
+  </div>
 </template>
 
 <script>
 // import HelloWorld from '../components/HelloWorld'
 import ExpansionPanel from '@/components/ExpansionPanel.vue'
+// import Draggable from '@/components/Draggable.vue'
 
 export default {
   components: {
     ExpansionPanel
+    // Draggable
   }
 }
 </script>
